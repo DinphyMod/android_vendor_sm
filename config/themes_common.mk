d78f2a380143ace6afbf26c4fc9605dc593d18b1
dinphy1205 <dinphy1205@126.com>
2017-03-08 20:54:26 +0800

    Change some config

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -1,9 +1,11 @@
 # Use all private apps
 PRODUCT_PACKAGES += \
+    AmazeFileManager \
     BaiduInput \
     Brevent \
     Browser \
     Music \
     NLauncher \
     NLnotifier \
-    SMWeatherProvider
+    SMWeatherProvider \
+    Venus
